b50a0c9ad35f5de6c23bc9ac0112ec87cb8361c7
Alexander Swensen <alex.swensen@gmail.com>
2023-04-11 20:04:38 -0500

    rename to example

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,5 @@
 import prisma from '$lib/prisma';
 import type { User } from '@prisma/client';
-import { json } from '@sveltejs/kit';
 
 export async function load(): Promise<{ users: User[] }> {
 	const users = await prisma.user.findMany();
