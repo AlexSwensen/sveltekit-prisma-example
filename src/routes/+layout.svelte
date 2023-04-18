dc077023abadcc0e1722087f015889c426c4da30
Alexander Swensen <alex.swensen@gmail.com>
2023-04-17 19:29:21 -0500

    add editorconfig and style light and dark with tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
-    import '../app.css'
+	import '../app.css';
 </script>
 
-
-<slot />
+<div class="h-screen w-screen bg-slate-200 p-5 dark:bg-slate-800">
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,24 @@
 	export let data: PageData;
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<h2>Users</h2>
-
-{#each data.users as user}
-	<div>
-		{user.name} -- {user.email}
+<div class="flex flex-col justify-center items-center">
+	<div class="mb-10">
+		<div class="rounded-lg bg-slate-700 p-10 text-slate-100 dark:bg-slate-500 dark:text-slate-100">
+			<h1 class="text-6xl">Welcome to SvelteKit</h1>
+			<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+			<p>We use Tailwind css on this page!</p>
+		</div>
 	</div>
-{/each}
+
+	<div class="bg-slate-700 text-slate-100 rounded-lg p-5 dark:bg-slate-500 dark:text-slate-100">
+		<h2 class="font-bold text-lg">List of Users:</h2>
+
+		<ul class="list-disc list-inside">
+			{#each data.users as user}
+				<li>
+					{user.name} -- {user.email}
+				</li>
+			{/each}
+		</ul>
+	</div>
+</div>
